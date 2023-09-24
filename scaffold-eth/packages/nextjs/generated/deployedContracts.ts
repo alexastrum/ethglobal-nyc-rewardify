82552354653334a63760a94fe2d55f16ca2c88be
Alex <alexastrum@gmail.com>
2023-09-24 08:58:52 -0400

    Updated contract abis

--- scaffold-eth/packages/nextjs/generated/deployedContracts.ts
+++ scaffold-eth/packages/nextjs/generated/deployedContracts.ts
@@ -1,11 +1,11 @@
 const contracts = {
-  31337: [
+  10200: [
     {
-      chainId: "31337",
-      name: "localhost",
+      chainId: "10200",
+      name: "chiado",
       contracts: {
         YourContract: {
-          address: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+          address: "0x1CB2c332D190EED0Ba8A61c81d68a82b91321aD9",
           abi: [
             {
               inputs: [],
@@ -119,6 +119,321 @@ const contracts = {
               name: "setDrop",
               outputs: [],
               stateMutability: "payable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "dropId",
+                  type: "address",
+                },
+                {
+                  internalType: "address",
+                  name: "receiver",
+                  type: "address",
+                },
+              ],
+              name: "triggerDrop",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "withdraw",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "withdrawAll",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              stateMutability: "payable",
+              type: "receive",
+            },
+          ],
+        },
+      },
+    },
+  ],
+  31337: [
+    {
+      chainId: "31337",
+      name: "localhost",
+      contracts: {
+        YourContract: {
+          address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+          abi: [
+            {
+              inputs: [],
+              stateMutability: "nonpayable",
+              type: "constructor",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: true,
+                  internalType: "address",
+                  name: "oldOwner",
+                  type: "address",
+                },
+                {
+                  indexed: true,
+                  internalType: "address",
+                  name: "newOwner",
+                  type: "address",
+                },
+              ],
+              name: "OwnerSet",
+              type: "event",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "newOwner",
+                  type: "address",
+                },
+              ],
+              name: "changeOwner",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "user",
+                  type: "address",
+                },
+              ],
+              name: "getDrop",
+              outputs: [
+                {
+                  components: [
+                    {
+                      internalType: "string",
+                      name: "triggerText",
+                      type: "string",
+                    },
+                    {
+                      internalType: "uint256",
+                      name: "amount",
+                      type: "uint256",
+                    },
+                    {
+                      internalType: "uint256",
+                      name: "cooldownSeconds",
+                      type: "uint256",
+                    },
+                    {
+                      internalType: "uint256",
+                      name: "deposit",
+                      type: "uint256",
+                    },
+                  ],
+                  internalType: "struct YourContract.Drop",
+                  name: "",
+                  type: "tuple",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "receiver",
+                  type: "address",
+                },
+              ],
+              name: "getLastAssertion",
+              outputs: [
+                {
+                  components: [
+                    {
+                      components: [
+                        {
+                          internalType: "bool",
+                          name: "arbitrateViaEscalationManager",
+                          type: "bool",
+                        },
+                        {
+                          internalType: "bool",
+                          name: "discardOracle",
+                          type: "bool",
+                        },
+                        {
+                          internalType: "bool",
+                          name: "validateDisputers",
+                          type: "bool",
+                        },
+                        {
+                          internalType: "address",
+                          name: "assertingCaller",
+                          type: "address",
+                        },
+                        {
+                          internalType: "address",
+                          name: "escalationManager",
+                          type: "address",
+                        },
+                      ],
+                      internalType:
+                        "struct OptimisticOracleV3Interface.EscalationManagerSettings",
+                      name: "escalationManagerSettings",
+                      type: "tuple",
+                    },
+                    {
+                      internalType: "address",
+                      name: "asserter",
+                      type: "address",
+                    },
+                    {
+                      internalType: "uint64",
+                      name: "assertionTime",
+                      type: "uint64",
+                    },
+                    {
+                      internalType: "bool",
+                      name: "settled",
+                      type: "bool",
+                    },
+                    {
+                      internalType: "contract IERC20",
+                      name: "currency",
+                      type: "address",
+                    },
+                    {
+                      internalType: "uint64",
+                      name: "expirationTime",
+                      type: "uint64",
+                    },
+                    {
+                      internalType: "bool",
+                      name: "settlementResolution",
+                      type: "bool",
+                    },
+                    {
+                      internalType: "bytes32",
+                      name: "domainId",
+                      type: "bytes32",
+                    },
+                    {
+                      internalType: "bytes32",
+                      name: "identifier",
+                      type: "bytes32",
+                    },
+                    {
+                      internalType: "uint256",
+                      name: "bond",
+                      type: "uint256",
+                    },
+                    {
+                      internalType: "address",
+                      name: "callbackRecipient",
+                      type: "address",
+                    },
+                    {
+                      internalType: "address",
+                      name: "disputer",
+                      type: "address",
+                    },
+                  ],
+                  internalType: "struct OptimisticOracleV3Interface.Assertion",
+                  name: "",
+                  type: "tuple",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "getOwner",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "dropId",
+                  type: "address",
+                },
+                {
+                  internalType: "address",
+                  name: "receiver",
+                  type: "address",
+                },
+                {
+                  internalType: "string",
+                  name: "assertedClaim",
+                  type: "string",
+                },
+              ],
+              name: "propareDropWithAssertedClaim",
+              outputs: [
+                {
+                  internalType: "bytes32",
+                  name: "",
+                  type: "bytes32",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "revertMyLastAssertion",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "string",
+                  name: "triggerText",
+                  type: "string",
+                },
+                {
+                  internalType: "uint256",
+                  name: "amount",
+                  type: "uint256",
+                },
+                {
+                  internalType: "uint256",
+                  name: "cooldownSeconds",
+                  type: "uint256",
+                },
+              ],
+              name: "setDrop",
+              outputs: [],
+              stateMutability: "payable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "settleMyLastAssertion",
+              outputs: [],
+              stateMutability: "nonpayable",
               type: "function",
             },
             {
