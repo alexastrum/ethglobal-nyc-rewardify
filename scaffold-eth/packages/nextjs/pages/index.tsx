d60dd9955c6f2fa681c88b24ae9769a85cc209c6
Alex <alexastrum@gmail.com>
2023-09-24 06:41:50 -0400

    Add WorldId verification

--- scaffold-eth/packages/nextjs/pages/index.tsx
+++ scaffold-eth/packages/nextjs/pages/index.tsx
@@ -1,71 +1,68 @@
-import Link from "next/link";
+// import Link from "next/link";
+import type { VerifyReply } from "./api/verify";
+import { CredentialType, IDKitWidget } from "@worldcoin/idkit";
+import type { ISuccessResult } from "@worldcoin/idkit";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
+// import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
+  const signal = JSON.stringify({});
+  const action = "auth";
+
+  const onSuccess = (result: ISuccessResult) => {
+    // This is where you should perform frontend actions once a user has been verified, such as redirecting to a new page
+    window.alert("Successfully verified with World ID! Your nullifier hash is: " + result.nullifier_hash);
+  };
+
+  const handleProof = async (result: ISuccessResult) => {
+    console.log("Proof received from IDKit:\n", JSON.stringify(result)); // Log the proof from IDKit to the console for visibility
+    const reqBody = {
+      merkle_root: result.merkle_root,
+      nullifier_hash: result.nullifier_hash,
+      proof: result.proof,
+      credential_type: result.credential_type,
+      action,
+      signal,
+    };
+    console.log("Sending proof to backend for verification:\n", JSON.stringify(reqBody)); // Log the proof being sent to our backend for visibility
+    const res: Response = await fetch("/api/verify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqBody),
+    });
+    const data: VerifyReply = await res.json();
+    if (res.status == 200) {
+      console.log("Successful response from backend:\n", data); // Log the response from our backend for visibility
+    } else {
+      throw new Error(`Error code ${res.status} (${data.code}): ${data.detail}` ?? "Unknown error."); // Throw an error if verification fails
+    }
+  };
+
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      WorldID app id: {process.env.NEXT_PUBLIC_WLD_APP_ID!} <br />
+      Action: {action} <br />
+      Signal: {signal} <br />
+      <IDKitWidget
+        action={action}
+        app_id={process.env.NEXT_PUBLIC_WLD_APP_ID!}
+        onSuccess={onSuccess}
+        handleVerify={handleProof}
+        credential_types={[CredentialType.Orb, CredentialType.Phone]}
+        signal={signal}
+        autoClose
+        enableTelemetry
+      >
+        {({ open }) => (
+          <button className="border border-black rounded-md" onClick={open}>
+            <div className="mx-3 my-1">Verify with World ID</div>
+          </button>
+        )}
+      </IDKitWidget>
     </>
   );
 };
